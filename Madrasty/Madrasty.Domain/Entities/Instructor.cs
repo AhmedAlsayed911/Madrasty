b4150e59a474a684eb958c6c3f4007c41adaf489
AhmedAlsayed911 <ahmedalasayed911@gmail.com>
2026-03-15 08:26:12 +0200

    add Instructor and InstructorSubjects entities

---
+++ Madrasty/Madrasty.Domain/Entities/Instructor.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Madrasty.Domain.Entities
+{
+    public class Instructor
+    {
+        public Instructor()
+        {
+            Instructors = new HashSet<Instructor>();
+            Ins_Subjects = new HashSet<InstructorSubjects>();
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int InsId { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Position { get; set; }
+        public int? SupervisorId { get; set; }
+        public decimal? Salary { get; set; }
+        public string? Image { get; set; }
+        public int DID { get; set; }
+        [ForeignKey(nameof(DID))]
+        [InverseProperty("Instructors")]
+        public virtual Department? Department { get; set; }
+
+        [InverseProperty("Instructor")]
+        public virtual Department? DepartmentManager { get; set; }
+
+
+        [ForeignKey(nameof(SupervisorId))]
+        [InverseProperty("Instructors")]
+        public virtual Instructor? Supervisor { get; set; }
+        [InverseProperty("Supervisor")]
+        public virtual ICollection<Instructor> Instructors { get; set; }
+
+        [InverseProperty("instructor")]
+        public virtual ICollection<InstructorSubjects> Ins_Subjects { get; set; }
+
+    }
+}
---
+++ Madrasty/Madrasty.Domain/Entities/InstructorSubjects.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Madrasty.Domain.Entities
+{
+    public class InstructorSubjects
+    {
+        [Key]
+        public int InsId { get; set; }
+        [Key]
+        public int SubId { get; set; }
+        [ForeignKey(nameof(InsId))]
+        [InverseProperty("Ins_Subjects")]
+        public virtual Instructor? instructor { get; set; }
+        [ForeignKey(nameof(SubId))]
+        [InverseProperty("Ins_Subjects")]
+        public virtual Subject? Subject { get; set; }
+
+    }
+}
