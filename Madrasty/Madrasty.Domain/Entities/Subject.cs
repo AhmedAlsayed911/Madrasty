dbd6c5aed5263c8e7cbd3e60caabee42c2889d57
AhmedAlsayed911 <ahmedalasayed911@gmail.com>
2026-03-15 08:26:00 +0200

    add Subject entity

--- Madrasty/Madrasty.Domain/Entities/Subject.cs
+++ Madrasty/Madrasty.Domain/Entities/Subject.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Madrasty.Domain.Entities
 {
@@ -10,15 +8,22 @@ namespace Madrasty.Domain.Entities
         public Subject()
         {
             StudentsSubjects = new HashSet<StudentSubject>();
-            DepartmentsSubjects = new HashSet<DepartmentSubject>();
+            DepartmetsSubjects = new HashSet<DepartmentSubject>();
+            Ins_Subjects = new HashSet<InstructorSubjects>();
         }
-
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int SubID { get; set; }
-        [StringLength(200)]
-        public string SubjectName { get; set; }
-        public DateTime Period { get; set;  }
+        [StringLength(500)]
+        public string? Name { get; set; }
+        public int? Period { get; set; }
+        [InverseProperty("Subject")]
         public virtual ICollection<StudentSubject> StudentsSubjects { get; set; }
-        public virtual ICollection<DepartmentSubject> DepartmentsSubjects { get; set; }
+        [InverseProperty("Subject")]
+        public virtual ICollection<DepartmentSubject> DepartmetsSubjects { get; set; }
+        [InverseProperty("Subject")]
+        public virtual ICollection<InstructorSubjects> Ins_Subjects { get; set; }
+
+
     }
 }
