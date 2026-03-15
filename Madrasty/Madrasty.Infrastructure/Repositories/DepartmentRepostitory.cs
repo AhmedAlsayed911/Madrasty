899d941fa4b452e978c10b2fbe7aec363289d640
AhmedAlsayed911 <ahmedalasayed911@gmail.com>
2026-03-15 08:26:43 +0200

    add DepartmentRepository implementation

---
+++ Madrasty/Madrasty.Infrastructure/Repositories/DepartmentRepostitory.cs
@@ -0,0 +1,17 @@
+﻿using Madrasty.Domain.Entities;
+using Madrasty.Infrastructure.Abstracts;
+using Madrasty.Infrastructure.Data;
+using Madrasty.Infrastructure.InfrastructureBases;
+using Microsoft.EntityFrameworkCore;
+
+namespace Madrasty.Infrastructure.Repositories
+{
+    public class DepartmentRepostitory : GenericRepositoryAsync<Department>, IDepartmentRepostitory
+    {
+        private DbSet<Department> Departments;
+        public DepartmentRepostitory(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            Departments = dbContext.Set<Department>();
+        }
+    }
+}
