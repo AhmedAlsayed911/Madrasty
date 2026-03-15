06c560c92d74280cff4cb9fa53c44b142b60b100
AhmedAlsayed911 <ahmedalasayed911@gmail.com>
2026-03-15 08:27:37 +0200

    add DepartmentController

---
+++ Madrasty/Madrasty.Api/Controllers/DepartmentController.cs
@@ -0,0 +1,19 @@
+﻿using Madrasty.Api.Base;
+using Madrasty.Core.Features.Department.Queries.Models;
+using Madrasty.Domain.AppMetaData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Madrasty.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : AppControllerBase
+    {
+        [HttpGet(Router.DepartmentRouting.GetById)]
+        public async Task<IActionResult> GetDepartmentById([FromQuery] GetDepartmentByIdQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return NewResult(result);
+        }
+    }
+}
