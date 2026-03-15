13e68a2a86b7c03bb64032296516de704657f9fa
AhmedAlsayed911 <ahmedalasayed911@gmail.com>
2026-03-15 08:26:52 +0200

    add InstructorRepository and SubjectRepository

---
+++ Madrasty/Madrasty.Infrastructure/Repositories/InstructorRepository.cs
@@ -0,0 +1,17 @@
+﻿using Madrasty.Domain.Entities;
+using Madrasty.Infrastructure.Abstracts;
+using Madrasty.Infrastructure.Data;
+using Madrasty.Infrastructure.InfrastructureBases;
+using Microsoft.EntityFrameworkCore;
+
+namespace Madrasty.Infrastructure.Repositories
+{
+    public class InstructorRepository : GenericRepositoryAsync<Instructor>, IINstructorRepostitory
+    {
+        private DbSet<Instructor> Instructors;
+        public InstructorRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            Instructors = dbContext.Set<Instructor>();
+        }
+    }
+}
---
+++ Madrasty/Madrasty.Infrastructure/Repositories/SubjectRepository.cs
@@ -0,0 +1,18 @@
+﻿using Madrasty.Domain.Entities;
+using Madrasty.Infrastructure.Abstracts;
+using Madrasty.Infrastructure.Data;
+using Madrasty.Infrastructure.InfrastructureBases;
+using Microsoft.EntityFrameworkCore;
+
+namespace Madrasty.Infrastructure.Repositories
+{
+    public class SubjectRepository : GenericRepositoryAsync<Subject>, ISubjectRepostitory
+
+    {
+        private DbSet<Subject> Subjects;
+        public SubjectRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            Subjects = dbContext.Set<Subject>();
+        }
+    }
+}
