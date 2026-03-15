c1220edc215fc86be9e11ba9b6dd738886447391
AhmedAlsayed911 <ahmedalasayed911@gmail.com>
2026-03-15 08:27:01 +0200

    add DepartmentService and IDepartmentService

---
+++ Madrasty/Madrasty.Service/Abstracts/IDepartmentService.cs
@@ -0,0 +1,9 @@
+﻿using Madrasty.Domain.Entities;
+
+namespace Madrasty.Service.Abstracts
+{
+    public interface IDepartmentService
+    {
+        Task<Department> GetDepartmentByid(int id);
+    }
+}
---
+++ Madrasty/Madrasty.Service/Services/DepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using Madrasty.Domain.Entities;
+using Madrasty.Infrastructure.Abstracts;
+using Madrasty.Service.Abstracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Madrasty.Service.Services
+{
+    public class DepartmentService(IDepartmentRepostitory repostitory)
+        : IDepartmentService
+    {
+        public async Task<Department> GetDepartmentByid(int id)
+        {
+            var result = await
+                repostitory
+                .GetTableNoTracking()
+                .Where(i => i.DID.Equals(id))
+                .FirstOrDefaultAsync();
+
+            return result;
+        }
+    }
+}
