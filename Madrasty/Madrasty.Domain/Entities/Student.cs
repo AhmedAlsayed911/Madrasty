1f0c02a506d6919cf8a6027a053a784dd5c6459f
AhmedAlsayed911 <ahmedalasayed911@gmail.com>
2026-03-15 08:25:52 +0200

    update Student entity

--- Madrasty/Madrasty.Domain/Entities/Student.cs
+++ Madrasty/Madrasty.Domain/Entities/Student.cs
@@ -5,17 +5,25 @@ namespace Madrasty.Domain.Entities
 {
     public class Student
     {
+        public Student()
+        {
+            StudentSubject = new HashSet<StudentSubject>();
+        }
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int StudID { get; set; }
-        [StringLength(200)]
-        public string Name { get; set; }
-        [StringLength(200)]
-        public string Address { get; set; }
+        public string? Name { get; set; }
         [StringLength(500)]
-        public string Phone { get; set; }
+        public string? Address { get; set; }
+        [StringLength(500)]
+        public string? Phone { get; set; }
         public int? DID { get; set; }
 
         [ForeignKey("DID")]
-        public virtual Department Department { get; set; }
+        [InverseProperty("Students")]
+        public virtual Department? Department { get; set; }
+        [InverseProperty("Student")]
+        public virtual ICollection<StudentSubject> StudentSubject { get; set; }
+
     }
 }
